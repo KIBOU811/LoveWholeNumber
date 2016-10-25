951ab9376bfd3fd7fefca01c7b05a36636a38c38
きーぼう <kibou811@gmail.com>
2016-10-25 23:59:13 +0900

    素数関連の関数の大量追加

--- Src/LoveWholeNumber/LoveWholeNumber/Class1.cs
+++ Src/LoveWholeNumber/LoveWholeNumber/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace LoveWholeNumber
 {
@@ -95,11 +96,222 @@ namespace LoveWholeNumber
 
         public static bool IsPalindromicNumber(int n)
         {
+            if (n < 0)
+                return false;
+
             int reversal = int.Parse(new string(n.ToString().ToCharArray().Reverse().ToArray()));
+
             if (reversal == n)
                 return true;
 
             return false;
+        }
+
+        public static bool IsPalindromicPrime(int n)
+        {
+            if (IsPalindromicNumber(n) && IsPrime(n))
+                return true;
+
+            return false;
+        }
+
+        public static bool IsEmirp(int n)
+        {
+            if (n < 0)
+                return false;
+
+            int reversal = int.Parse(new string(n.ToString().ToCharArray().Reverse().ToArray()));
+            
+            if(!IsPalindromicNumber(n)&&IsPrime(reversal))
+                return true;
+
+            return false;
+        }
+
+        public static bool IsEularPrimeNumber(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int compNum = i ^ 2 + i + 41;
+
+                if (compNum == n && IsPrime(compNum))
+                    return true;
+
+                if (compNum > n)
+                    break;
+            }
+
+            return false;
+        }
+
+        public static bool IsSuperPrime(int n)
+        {
+            if (IsPrime(n) && IsPrime(NumOfPrime(n)))
+                return true;
+
+            return false;
+        }
+
+        public static bool IsMersenneNumber(int n)
+        {
+            if (n == 2147483647)
+                return true;
+
+            if (n < 0)
+                return false;
+
+            for (int i = 1; i < 31; i++)
+            {
+                int checkNum = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    checkNum *= 2;
+                }
+                if (checkNum == n)
+                    return true;
+
+                if (checkNum > n)
+                    return false;
+            }
+
+            return false;
+        }
+
+        public static bool IsMersennePrime(int n)
+        {
+            if (IsMersenneNumber(n) && IsPrime(n))
+                return true;
+
+            return false;
+        }
+
+        public static bool IsSophieGermainPrime(int n)
+        {
+            if (IsPrime(n) && IsPrime(2 * n + 1))
+                return true;
+
+            return false;
+        }
+
+        public static bool IsSafePrime(int n)
+        {
+            if (n % 2 != 0)
+                return false;
+
+            if (IsPrime(n) && IsPrime((n - 1) / 2))
+                return true;
+
+            return false;
+        }
+
+        public static bool IsCullenNumber(int n)
+        {
+            if (n < 0)
+                return false;
+
+            for (int i = 1; i < 27; i++)
+            {
+                int checkNum = i;
+                for (int j = 0; j < i; j++)
+                {
+                    checkNum *= 2;
+                }
+                checkNum += 1;
+
+                if (checkNum == n)
+                    return true;
+
+                if (checkNum > n)
+                    return false;
+            }
+
+            return false;
+        }
+
+        public static bool IsCullenPrime(int n)
+        {
+            if (IsCullenNumber(n) && IsPrime(n))
+                return true;
+
+            return false;
+        }
+
+        public static bool IsWoodallNumber(int n)
+        {
+            if (n < 0)
+                return false;
+
+            for (int i = 1; i < 27; i++)
+            {
+                int checkNum = i;
+                for (int j = 0; j < i; j++)
+                {
+                    checkNum *= 2;
+                }
+                checkNum -= 1;
+
+                if (checkNum == n)
+                    return true;
+
+                if (checkNum > n)
+                    return false;
+            }
+
+            return false;
+        }
+
+        public static bool IsWoodallPrime(int n)
+        {
+            if (IsWoodallNumber(n) && IsPrime(n))
+                return true;
+
+            return false;
+        }
+
+        public static bool IsOddPrime(int n)
+        {
+            if (IsPrime(n) && n != 2)
+                return true;
+
+            return false;
+        }
+
+        public static bool IsEvenPrime(int n)
+        {
+            if(IsPrime(n)&&n==2)
+                return true;
+
+            return false;
+        }
+
+        public static List<int> ListUpDivisor(int n)
+        {
+            List<int> divisors = new List<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    divisors.Add(i);
+            }
+            return divisors;
+        }
+
+        public static int CountOfDivisor(int n)
+        {
+            return ListUpDivisor(n).Count;
+        }
+
+
+
+
+
+        public static int SumOfDivisors(List<int> Divisors)
+        {
+            int sum = 0;
+
+            foreach (int d in Divisors)
+                sum += d;
+
+            return sum;
         }
     }
 }
