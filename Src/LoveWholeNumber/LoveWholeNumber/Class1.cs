09163dff73b5a9499908df0521befd786adad4d4
きーぼう <kibou811@gmail.com>
2016-11-02 23:05:22 +0900

    [add]平衡素数判定メソッドの追加

--- Src/LoveWholeNumber/LoveWholeNumber/Class1.cs
+++ Src/LoveWholeNumber/LoveWholeNumber/Class1.cs
@@ -746,5 +746,23 @@ namespace LoveWholeNumber
 
             return false;
         }
+
+        /// <summary>
+        /// 平衡素数判定
+        /// </summary>
+        /// <param name="n">判定したい自然数</param>
+        /// <returns>平衡素数ならtrue でないならfalse</returns>
+        public static bool IsBalancedPrime(int n)
+        {
+            int numOfPrime = GetNumOfPrime(n);
+
+            if (numOfPrime == -1)
+                return false;
+
+            if ((GetNthPrime(numOfPrime - 1) + GetNumOfPrime(numOfPrime + 1)) / 2 == n)
+                return true;
+
+            return false;
+        }
     }
 }
