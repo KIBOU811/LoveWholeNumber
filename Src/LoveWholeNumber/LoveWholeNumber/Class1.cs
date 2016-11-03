d58767355dff6807aee615b1beb317935d924771
きーぼう <kibou811@gmail.com>
2016-11-03 20:50:56 +0900

    [add]過剰数判定メソッドの追加

--- Src/LoveWholeNumber/LoveWholeNumber/Class1.cs
+++ Src/LoveWholeNumber/LoveWholeNumber/Class1.cs
@@ -281,5 +281,33 @@ namespace LoveWholeNumber
 
             return false;
         }
+
+        /// <summary>
+        /// 過剰数判定
+        /// </summary>
+        /// <param name="n">判定したい自然数</param>
+        /// <returns>過剰数ならtrue でないならfalse</returns>
+        public static bool IsAbundantNumber(int n)
+        {
+            List<int> divisorList = ListUpDivisor(n);
+
+            if (divisorList.Count == 0)
+                return false;
+
+            int sum = new int();
+
+            foreach (var d in divisorList)
+            {
+                if (d == n)
+                    break;
+
+                sum += d;
+            }
+
+            if (sum > n)
+                return true;
+
+            return false;
+        }
     }
 }
