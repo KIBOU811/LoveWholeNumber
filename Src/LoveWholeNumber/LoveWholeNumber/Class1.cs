4767b9f860ba0fd148fd4c56fcf617764663ae24
きーぼう <kibou811@gmail.com>
2016-11-11 18:00:18 +0900

    [add]六角数判定メソッドの追加

--- Src/LoveWholeNumber/LoveWholeNumber/Class1.cs
+++ Src/LoveWholeNumber/LoveWholeNumber/Class1.cs
@@ -1079,5 +1079,31 @@ namespace LoveWholeNumber
 
             return false;
         }
+
+        /// <summary>
+        /// 六角数判定
+        /// </summary>
+        /// <param name="n">判定したい自然数</param>
+        /// <returns>六角数ならtrue でないならfalse</returns>
+        public static bool IsHexagonalNumber(int n)
+        {
+            if (n < 1)
+                return false;
+
+            int toN = 1;
+
+            for (int i = 1; i < int.MaxValue; i++)
+            {
+                toN += 4 * i + 1;
+
+                if (toN == n)
+                    return true;
+
+                if (toN > n)
+                    break;
+            }
+
+            return false;
+        }
     }
 }
