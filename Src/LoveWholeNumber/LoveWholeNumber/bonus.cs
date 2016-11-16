e25d43e8c62ff607807d734c9179cd3b2e32ec19
きーぼう <kibou811@gmail.com>
2016-11-16 22:41:26 +0900

    [clean]ツェラーの公式を用いたA.D.4以降の曜日取得メソッド内の変数名のリファクタリング

--- Src/LoveWholeNumber/LoveWholeNumber/bonus.cs
+++ Src/LoveWholeNumber/LoveWholeNumber/bonus.cs
@@ -14,9 +14,9 @@ namespace LoveWholeNumber
         /// <param name="year">年</param>
         /// <param name="month">月</param>
         /// <param name="day">日</param>
-        /// <param name="isISO8601">返り値をISO8601で返すか</param>
+        /// <param name="isIso8601">返り値をISO8601で返すか</param>
         /// <returns>曜日を表す数字</returns>
-        public static int GetDayOfTheWeek(int year, int month, int day, bool isISO8601)
+        public static int GetDayOfTheWeek(int year, int month, int day, bool isIso8601)
         {
             if (year < 4)
                 return -1;
@@ -34,15 +34,15 @@ namespace LoveWholeNumber
                 g = -2 * c + (int) Math.Floor((double)c / 4);
             }
 
-            int y = year % 100;
+            int yr = year % 100;
 
-            if (isISO8601)
+            if (isIso8601)
             {
-                return (day + (int) Math.Floor((double) 26 * (month + 1) / 10) + y + (int) Math.Floor((double) y / 4) +
+                return (day + (int) Math.Floor((double) 26 * (month + 1) / 10) + yr + (int) Math.Floor((double) yr / 4) +
                         g + 5) % 7 + 1;
             }
 
-            return (day + (int) Math.Floor((double) 26 * (month + 1) / 10) + y + (int) Math.Floor((double) y / 4) +
+            return (day + (int) Math.Floor((double) 26 * (month + 1) / 10) + yr + (int) Math.Floor((double) yr / 4) +
                         g) % 7;
         }
     }
