4b5ce11f47224fd28bf52cc1beb4f99e977470be
きーぼう <kibou811@gmail.com>
2016-11-18 21:23:03 +0900

    [add]グレゴリオ暦での曜日取得メソッドの追加

--- Src/LoveWholeNumber/LoveWholeNumber/bonus.cs
+++ Src/LoveWholeNumber/LoveWholeNumber/bonus.cs
@@ -41,5 +41,33 @@ namespace LoveWholeNumber
             return (day + (int) Math.Floor((double) 26 * (month + 1) / 10) + yr + (int) Math.Floor((double) yr / 4) +
                         g) % 7;
         }
+
+        /// <summary>
+        /// グレゴリオ暦での曜日取得
+        /// </summary>
+        /// <param name="year">年</param>
+        /// <param name="month">月</param>
+        /// <param name="day">日</param>
+        /// <param name="isIso8601">返り値をISO8601で返すか</param>
+        /// <returns>曜日を表す数字</returns>
+        public static int GetDayWeekByGregorian(int year, int month, int day, bool isIso8601)
+        {
+            if (year < 1)
+                year--;
+
+            int c = (int) Math.Floor((double) year / 100);
+            int g = -2 * c + (int) Math.Floor((double) c / 4);
+
+            int yr = year % 100;
+
+            if (isIso8601)
+            {
+                return (day + (int) Math.Floor((double) 26 * (month + 1) / 10) + yr + (int) Math.Floor((double) yr / 4) +
+                        g + 5) % 7 + 1;
+            }
+
+            return (day + (int) Math.Floor((double) 26 * (month + 1) / 10) + yr + (int) Math.Floor((double) yr / 4) +
+                    g) % 7;
+        }
     }
 }
